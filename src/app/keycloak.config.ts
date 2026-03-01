3b705b592d81a4b232485233d48ca902c18306c5
Michael1roger <54472407+root-gift@users.noreply.github.com>
2026-03-01 09:42:46 +0000

    Changement de l'url

--- src/app/keycloak.config.ts
+++ src/app/keycloak.config.ts
@@ -2,8 +2,8 @@ import { KeycloakConfig } from 'keycloak-js';
 
 export const keycloakConfig: KeycloakConfig = {
 
-  // url: 'http://192.168.11.104:8180',
-  url: 'http://localhost:8180',
+  url: 'http://192.168.11.104:8180',
+  //url: 'http://localhost:8180',
   realm: 'sygepip',
   clientId: 'sygepip-frontend',
 };
